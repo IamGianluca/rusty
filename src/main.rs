2b258c98ac2f63355459fe1cd1df957aea78cdae
Gianluca Rossi <gr.gianlucarossi@gmail.com>
2023-08-27 16:54:45 -0400

    feat: add factory to create Users

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
+mod chat;
 mod user;
 
 fn main() {
     println!("I'm a little rusty with Rust!");
 
-    let luca = user::User {
-        name: String::from("luca"),
-    };
+    let luca = user::create_user(String::from("luca"));
     println!("Hi, my name is {0}", luca.name)
 }
--- src/user.rs
+++ src/user.rs
@@ -1,17 +1,18 @@
 pub struct User {
     pub name: String,
 }
+pub fn create_user(name: String) -> User {
+    User { name }
+}
 
 #[cfg(test)]
 mod tests {
 
-    use crate::user::User;
+    use crate::user::create_user;
 
     #[test]
     fn test_create_user() {
-        let user1 = User {
-            name: String::from("user1"),
-        };
+        let user1 = create_user(String::from("user1"));
         assert_eq!(user1.name, "user1")
     }
 }
