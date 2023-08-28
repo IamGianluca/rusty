bdacb4458a6140cf8eb08d13c275863287146d53
Gianluca Rossi <gr.gianlucarossi@gmail.com>
2023-08-28 00:00:54 -0400

    fix: forgot a semicolon

--- src/main.rs
+++ src/main.rs
@@ -5,5 +5,5 @@ fn main() {
     println!("I'm a little rusty with Rust!");
 
     let luca = user::create_user(String::from("luca"));
-    println!("Hi, my name is {0}", luca.name)
+    println!("Hi, my name is {0}", luca.name);
 }
