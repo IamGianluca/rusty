4fca9fee097a64a8d43d168d8605d829d10b9bdc
Gianluca Rossi <gr.gianlucarossi@gmail.com>
2023-08-29 22:34:10 -0400

    feat: use 'HashMap' to store users and rename 'Chat' to 'ChatClient'

--- src/chat.rs
+++ src/chat.rs
@@ -1,39 +1,57 @@
-use crate::user::User;
+use std::collections::HashMap;
 
-pub struct Chat {
+use crate::User;
+
+#[derive(Debug)]
+pub struct ChatClient {
     namespace: String,
-    users: Vec<User>,
+    users: HashMap<String, User>,
 }
-impl Chat {
-    fn new(namespace: String) -> Chat {
-        Chat {
+
+impl ChatClient {
+    fn new(namespace: String) -> ChatClient {
+        ChatClient {
             namespace,
-            users: Vec::new(),
+            users: HashMap::new(),
         }
     }
     fn register_user(&mut self, user: User) {
-        self.users.push(user)
+        self.users.insert(user.name.to_string(), user);
     }
 }
 
 #[cfg(test)]
 mod test {
 
-    use crate::chat::Chat;
+    use crate::chat::ChatClient;
     use crate::user::User;
 
     #[test]
     fn test_create_chat() {
-        let chat1 = Chat::new(String::from("company1"));
+        let chat1 = ChatClient::new(String::from("company1"));
         assert_eq!(chat1.namespace, "company1");
     }
 
     #[test]
     fn test_register_user() {
-        let mut chat1 = Chat::new(String::from("company1"));
+        let mut chat = ChatClient::new(String::from("company1"));
         let user1 = User::new(String::from("user1"));
-        chat1.register_user(user1);
-        assert_eq!(chat1.users.len(), 1);
-        assert_eq!(chat1.users[0].name, "user1");
+        chat.register_user(user1);
+        assert_eq!(chat.users.len(), 1);
+        assert_eq!(chat.users.contains_key("user1"), true);
+    }
+    #[test]
+    fn test_register_multiple_users() {
+        let mut chat = ChatClient::new(String::from("company1"));
+        let user1 = User::new(String::from("user1"));
+        let user2 = User::new(String::from("user2"));
+        let user3 = User::new(String::from("user3"));
+        chat.register_user(user1);
+        chat.register_user(user2);
+        chat.register_user(user3);
+        assert_eq!(chat.users.len(), 3);
+        for user_id in ["user1", "user2", "user3"] {
+            assert_eq!(chat.users.contains_key(user_id), true)
+        }
     }
 }
--- src/user.rs
+++ src/user.rs
@@ -1,6 +1,8 @@
+#[derive(Debug)]
 pub struct User {
     pub name: String,
 }
+
 impl User {
     pub fn new(name: String) -> User {
         User { name }
