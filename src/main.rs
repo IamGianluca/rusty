93cb3d596b3fd59fd5badb412b4b73cc77b1b15f
Gianluca Rossi <gr.gianlucarossi@gmail.com>
2023-10-05 11:15:24 -0400

    feat: send message via http request

--- src/main.rs
+++ src/main.rs
@@ -12,37 +12,50 @@ async fn hello() -> impl Responder {
 }
 
 #[derive(Deserialize)]
-struct Info {
+struct UserPayload {
     username: String,
     email: String,
 }
 
 #[post("/user")]
-async fn user(info: web::Json<Info>) -> impl Responder {
-    let conn = &mut crate::adapters::utils::get_database_connection();
-    let repo = &mut crate::adapters::user_repository::DbUserRepository { conn: conn };
-
+async fn user(info: web::Json<UserPayload>) -> impl Responder {
     let user = crate::domain::user::NewUser {
         username: &info.username,
         email: &info.email,
     };
+
+    let conn = &mut crate::adapters::utils::get_database_connection();
+    let repo = &mut crate::adapters::user_repository::DbUserRepository { conn };
     service_layer::service::create_user(user, repo);
+
     HttpResponse::Ok()
 }
 
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!")
+#[derive(Deserialize)]
+struct MessagePayload {
+    user_id: String,
+    channel_id: String,
+    content: String,
+}
+#[post("/message")]
+async fn message(info: web::Json<MessagePayload>) -> impl Responder {
+    let message = crate::domain::message::NewMessage {
+        channel_id: &info.channel_id.parse::<i32>().unwrap(),
+        user_id: &info.user_id.parse::<i32>().unwrap(),
+        content: &info.content,
+    };
+
+    let conn = &mut crate::adapters::utils::get_database_connection();
+    let repo = &mut crate::adapters::message_repository::DbMessageRepository { conn };
+    service_layer::service::send_message(message, repo);
+
+    HttpResponse::Ok()
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .service(hello)
-            .service(user)
-            .route("/hey", web::get().to(manual_hello))
-    })
-    .bind(("127.0.0.1", 8080))?
-    .run()
-    .await
+    HttpServer::new(|| App::new().service(hello).service(user).service(message))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
 }
