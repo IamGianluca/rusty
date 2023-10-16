8e3876758b5ec7ab1103be336e37608c06cbfca2
Gianluca Rossi <gr.gianlucarossi@gmail.com>
2023-10-16 12:43:48 -0400

    test: create end-to-end test for creating new user via HTTP request

--- tests/end_to_end_tests.rs
+++ tests/end_to_end_tests.rs
@@ -1,4 +1,5 @@
 use actix_web::{test, App};
+use serde_json::json;
 
 #[actix_web::test]
 async fn test_server_is_running() {
@@ -7,6 +8,27 @@ async fn test_server_is_running() {
 
     // when
     let req = test::TestRequest::get().uri("/").to_request();
+    let resp = test::call_service(&app, req).await;
+
+    // then
+    assert!(resp.status().is_success());
+}
+
+#[actix_web::test]
+async fn test_add_user_endpoint() {
+    // given
+    let app = test::init_service(App::new().service(rusty::add_user_endpoint)).await;
+
+    // when
+    let payload = json!({
+            "username": "Doe John",
+            "password": "password",
+            "email": "johnxxxx@example.com"
+    });
+    let req = test::TestRequest::post()
+        .uri("/user")
+        .set_json(&payload)
+        .to_request();
     let resp = test::call_service(&app, req).await;
 
     // then
