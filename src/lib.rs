d85840084f6fe5ba1f7836f44340c6df60cb8b9b
Gianluca Rossi <gr.gianlucarossi@gmail.com>
2023-10-29 10:46:21 -0400

    chore: remove unused code

--- src/lib.rs
+++ src/lib.rs
@@ -73,22 +73,6 @@ async fn update_credentials_endpoint(
     Ok(HttpResponse::Ok().finish())
 }
 
-// #[post("/authenticate")]
-// async fn authenticate_user_endpoint(
-//     data: web::Json<LoginPayload>,
-//     // creds: BearerAuth,
-// ) -> Result<HttpResponse> {
-//     let conn = &mut crate::adapters::utils::get_db_conn();
-//     let repo = &mut crate::adapters::user_repository::DbUserRepository { conn };
-//     // let r = service_layer::authentication::validate_token(&creds.token());
-//     // if r.is_err() {
-//     //     return Ok(HttpResponse::Forbidden().finish());
-//     // }
-//     service_layer::authentication::authenticate_user(&data.username, &data.password, repo);
-//     service_layer::authentication::create_token();
-//     Ok(HttpResponse::Ok().finish())
-// }
-
 #[derive(Deserialize)]
 struct ChannelPayload {
     name: String,
